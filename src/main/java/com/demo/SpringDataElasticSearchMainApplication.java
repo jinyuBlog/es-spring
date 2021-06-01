8036219d8ca11a18e28068029ab1ffdd47cb218f
jinyu <1546560862@qq.com>
2021-06-01 19:24:12 +0800

    Initial commit

---
+++ src/main/java/com/demo/SpringDataElasticSearchMainApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticSearchMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataElasticSearchMainApplication.class,args);
+    }
+}
